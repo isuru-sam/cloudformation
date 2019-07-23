95f5cd59736bf7ad2e1f61af642ef4f734fafbbe
Isuru Samaraweera <isurus@342-MC-C02YJ21LJG5H.local>
2019-07-23 11:03:20 +0530

    s4

--- serverless/deploy.sh
+++ serverless/deploy.sh
@@ -9,9 +9,10 @@ npm install serverless-dynamodb-local serverless-offline serverless-stack-output
 echo "Deploying to $env"
 echo "_______________________________"
 ls  -ls
+echo 'here1'
 serverless config credentials --provider aws --key AKIAS7MH6ILBJGZH5JY2 --secret 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
-
+echo 'here2'
 serverless package --stage dev -p $CODEBUILD_SRC_DIR/serverless/artifacts/$env
-
+echo 'here3'
 ls -ls $CODEBUILD_SRC_DIR/serverless/artifacts/$env
 serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/serverless/artifacts/$env -v
