e1d2f83b92ac30bff4339de2efd07c5120c2ca3c
Isuru Samaraweera <isurus@342-MC-C02YJ21LJG5H.local>
2019-07-23 10:43:52 +0530

    serverless1

--- serverless/deploy.sh
+++ serverless/deploy.sh
@@ -11,5 +11,6 @@ echo "_______________________________"
 ls  -ls
 serverless config credentials --provider aws --key AKIAS7MH6ILBJGZH5JY2 --secret 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
 
+serverless package --stage dev -p $CODEBUILD_SRC_DIR/serverless/artifacts/$env
 
 serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/serverless/artifacts/$env -v
